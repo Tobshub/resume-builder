ecf628203953f32d15bdca72ef978aac68020d4f
Tobani <tobanigeorge@gmail.com>
2023-01-30 19:46:30 +0100

    added login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 import BuilderPage from "./builder";
 import { appQueryClient, appTRPCClient } from "./context/query";
 import LandingPage from "./landing";
 import trpc from "./utils/trpc";
+import * as storage from "./lib/localstorage";
+import Login from "./auth";
 
 const router = createBrowserRouter([
   {
     path: "/builder",
     element: <BuilderPage />,
+    loader: () => {
+      const token = storage.get("token");
+      if (!token) {
+        return redirect("/auth");
+      }
+      return token;
+    },
+  },
+  {
+    path: "/auth",
+    element: <Login />,
   },
   {
     path: "/",
