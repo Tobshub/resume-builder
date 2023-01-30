fd702139bcafd7652a96c9a133f473344cdf63a2
Tobani <tobanigeorge@gmail.com>
2023-01-30 07:03:56 +0100

    added middleware export

--- server/src/config/trpc.ts
+++ server/src/config/trpc.ts
@@ -1,7 +1,8 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 
-const trpc = initTRPC.create();
+const trpc = initTRPC.context<{ auth: { token: string } }>().create();
 
 export const tRouter = trpc.router;
 export const tProcedure = trpc.procedure;
+export const tMiddleWare = trpc.middleware;
 export const tError = TRPCError;
